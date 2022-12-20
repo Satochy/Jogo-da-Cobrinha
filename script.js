2618c2dbf76e25f8ad80d26132ff64caa91def32
Satochy <matheusgamer326@gmail.com>
2022-12-20 18:20:29 -0300

    Criar cobrinha
    adição da cobrinha

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ function criarBG() {
 function criarCobrinha() {
     for(i=0; i < snake.length; i++){
         context.fillStyle="green";
-        context.fillRect(snake[i].x. snake[i].y, box, box);
+        context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
-criarBG();  
-criarCobrinha();
+criarBG ();  
+criarCobrinha ();
