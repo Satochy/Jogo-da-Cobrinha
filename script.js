0a06a1cc682a6b170a8d54408cf7cd5bd22738e8
Satochy <matheusgamer326@gmail.com>
2022-12-22 02:32:35 -0300

    coreção de bugs de moimentação e adição do GAME OVER

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+
 let direction = "rigth";
 let food ={
     x: Math.floor(Math.random() * 15 + 1) * box,
@@ -32,10 +33,10 @@ function drawFood (){
 document.addEventListener('keydown', update);
 
 function update (event){
-    if(event.keyCode == 37 && direction != "rigth") direction = "left";
-    if(event.keyCode == 38 && direction != "down") direction = "up";
-    if(event.keyCode == 39 && direction != "left") direction = "rigth";
-    if(event.keyCode == 40 && direction != "up") direction = "down";
+    if(event.keyCode == 37 && direction != 'right') direction = 'left';
+    if(event.keyCode == 38 && direction != 'down') direction = 'up';
+    if(event.keyCode == 39 && direction != 'left') direction = 'right';
+    if(event.keyCode == 40 && direction != 'up') direction = 'down';
 }
 
 function iniciarJogo(){
@@ -43,6 +44,13 @@ function iniciarJogo(){
     if(snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
     if(snake[0].y > 15*box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert (" GAME OVER ");
+        }
+    }
 
     criarBG ();  
     criarCobrinha ();
@@ -63,8 +71,6 @@ function iniciarJogo(){
         food.x = Math.floor(Math.random() * 15 + 1) * box;
         food.y = Math.floor(Math.random() * 15 + 1) * box;
     }
-
-   
 
     let newHead = {
         x: snakeX,
